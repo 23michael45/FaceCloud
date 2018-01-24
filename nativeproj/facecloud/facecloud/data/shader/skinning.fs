d77ea80789ff1948525e6acd1fc670dcb64e24c0
23michael45 <michael2345@live.cn>
2018-01-24 16:14:00 +0800

    add a folder where put real data

--- nativeproj/facecloud/facecloud/data/shader/skinning.fs
+++ nativeproj/facecloud/facecloud/data/shader/skinning.fs
@@ -141,8 +141,10 @@ void main()
                                                                                      
     vec4 color = texture(gColorMap, In.TexCoord.xy);
 	vec4 detail = texture(gDetailMap, In.TexCoord_C2.xy);
-	                   
+			
+	//TotalLight = vec4(1.0, 0.0, 0.0, 1.0);
+		 
     float blend = 0.5;					   
-    FragColor = (color * blend + detail * (1 - blend))* TotalLight;
-	
+    FragColor = (color * blend + detail * (1 - blend)) * TotalLight;
+	//FragColor = color * blend + detail * (1 - blend);
 }
