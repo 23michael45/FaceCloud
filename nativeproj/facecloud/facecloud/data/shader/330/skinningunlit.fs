5bab7b67ecc259c065ec1905a09252aa6ff65485
23michael45 <michael2345@live.cn>
2018-02-07 14:04:35 +0800

    glsl 1.2 version shader

--- nativeproj/facecloud/facecloud/data/shader/330/skinningunlit.fs
+++ nativeproj/facecloud/facecloud/data/shader/330/skinningunlit.fs
@@ -1,8 +1,8 @@
-#version 120
+#version 330
 
-varying  vec2 TexCoord0;
-varying  vec3 Normal0;    
-varying  vec2 TexCoord1;                                                                
+in vec2 TexCoord0;
+in vec3 Normal0;    
+in vec2 TexCoord1;                                                                
 
 struct VSOutput
 {
@@ -17,7 +17,7 @@ uniform sampler2D gDetailMap;
 uniform sampler2D gMaskMap;
                                                                                  
                                 
-//out vec4 FragColor;
+out vec4 FragColor;
                                                                 
 void main()
 {                                    
@@ -28,16 +28,16 @@ void main()
                                           
                                                                                                 
                                                                                      
-    vec4 color = texture2D(gColorMap, In.TexCoord.xy);                               
-    vec4 mask = texture2D(gMaskMap, In.TexCoord.xy);
-	vec4 detail = texture2D(gDetailMap, In.TexCoord_C2.xy);
+    vec4 color = texture(gColorMap, In.TexCoord.xy);                               
+    vec4 mask = texture(gMaskMap, In.TexCoord.xy);
+	vec4 detail = texture(gDetailMap, In.TexCoord_C2.xy);
 			
     float blend = (mask.x + mask.y + mask.z)/3;	
 	if(blend >= 0.991)
 	{
-		//color = vec4(0,0,0,0);
+		color = vec4(0,0,0,0);
 	}
-    gl_FragColor  = (color * blend + detail * (1 - blend));
+    FragColor = (color * blend + detail * (1 - blend));
 	
     //FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
